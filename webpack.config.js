0c434afe78e85ccf1224d976efc689660341630d
USER <user@USERs-MacBook-Air.local>
2021-11-01 09:49:41 -0500

    [FIX] webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,7 @@ module.exports = {
     entry: './src/index.js',
     output:{
         path: path.resolve(__dirname,'build'),
-        filename: 'bundle.js',
-        publicPath: '/'
+        filename: 'bundle.js'
     },
     mode: 'development',
     resolve: {
