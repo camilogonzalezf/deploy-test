87cdbfa69f6411674fe4eafd79a25a389a437ae9
Andres Benitez <abenitez@lululemon.com>
2021-11-09 20:39:19 -0500

    change path html 2

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: './src/index.js',
     output:{
-        publicPath: '/',
         path: path.resolve(__dirname,'build'),
         filename: 'bundle.js'
     },
