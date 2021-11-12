6043cbadfbe12fc687932fe8a413cc30c941a462
Andres Benitez <abenitez@lululemon.com>
2021-11-12 14:30:53 -0500

    test 8

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: `${__dirname}/src/index.js`,
     output:{
-      path: `${__dirname}/deploy-test`,
+      path: `${__dirname}/`,
       publicPath: '/deploy-test/',
       filename: 'bundle.js',
     },
