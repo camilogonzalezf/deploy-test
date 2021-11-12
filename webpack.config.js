8f120bcbb3c0e5d33c14c2be951e77c5f7d8fa32
Andres Benitez <abenitez@lululemon.com>
2021-11-12 15:11:36 -0500

    test 11

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     entry: `${__dirname}/src/index.js`,
     output:{
       path: `${__dirname}/`,
+      publicPath: '/deploy-test/',
       filename: 'bundle.js',
     },
     mode: 'development',
