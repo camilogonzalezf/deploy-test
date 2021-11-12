96eaf51d97fdd4d56f027cc145132600fb3f402a
Andres Benitez <abenitez@lululemon.com>
2021-11-12 14:20:25 -0500

    test 5

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: `${__dirname}/src/index.js`,
     output:{
       path: `${__dirname}/deploy-test`,
-      publicPath: '/deploy-test',
+      publicPath: '/deploy-test/',
       filename: 'bundle.js',
     },
     mode: 'development',
